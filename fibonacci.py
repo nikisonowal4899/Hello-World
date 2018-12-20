e6f2179ca279f348fd0b3e477d416e09c9cfe54d
nikisonowal4899 <nikisonowal4899@gmail.com>
2018-12-20 20:33:38 +0530

    Recursive function used

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+print("Programme to print first n fibbonacci number")
+n = int(input("Enter the value of n: "))
+
+def fib(n):
+	if n > 0:
+		(n1,n2) = fib(n-1)
+		nth = n1 + n2
+		print(nth)
+		return nth, n1 #returning (n-1)th and (nth) value
+
+	else:
+		print(0)
+		return 0,1
+
+
+fib(n-1) #calling the function for the first time
+
+
